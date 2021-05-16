ba4b020cdccc0ebd4cb50d2875b1f7aa18f45b44
Lydia-b <81586062+Lydia-b@users.noreply.github.com>
2021-05-16 15:09:32 +0200

    Weather icon included

--- src/index.js
+++ src/index.js
@@ -38,6 +38,10 @@ function showCurrentTemperature(response) {
     
     let h3Element = document.querySelector("#current-date-time");
     h3Element.innerHTML = formatDate(response.data.dt * 1000);
+
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 //Enter city search
